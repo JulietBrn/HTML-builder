2ec480d11e03296888419b480178b25aa1dae121
JulietBrn <shishkina.ja@gmail.com>
2024-01-18 16:23:56 +0100

    feat: complete first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt')
+const stream = fs.createReadStream(filePath)
+
+stream.on('data', (chunk) => {
+  process.stdout.write(chunk)
+})
