470f22dd3a67a76ce5639f49129156ad428136d9
JulietBrn <shishkina.ja@gmail.com>
2024-01-19 10:12:21 +0100

    feat: complete 02 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const greeting = 'Hi, please enter some text: ';
+const buy = 'Bye-bye! You are awesome!';
+
+const filePath = path.join(__dirname, 'text.txt');
+let writtenStream = fs.createWriteStream(filePath, { flags: 'a' });
+const readStream = fs.createReadStream(filePath);
+
+let rl = readline.createInterface(process.stdin, process.stdout);
+
+console.log(greeting);
+function askForInput() {
+  rl.question('', (answer) => {
+    if (answer === 'exit') {
+      console.log(buy);
+      // readStream.push(answer)
+      rl.close();
+      process.exit();
+    }
+    writtenStream.write(`${answer} `);
+    askForInput();
+  });
+}
+
+askForInput();
+
+rl.on('SIGINT', () => {
+  console.log(buy);
+  rl.close();
+  process.exit();
+});
