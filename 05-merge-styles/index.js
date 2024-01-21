331d3997bd9d97e4edb3e8fd9ac8438067706a72
JulietBrn <shishkina.ja@gmail.com>
2024-01-21 12:13:53 +0100

    feat: complete 6 task and fix 5 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -14,18 +14,27 @@ fs.readdir(folderSource,
     else {
       files.forEach(file => {
         const filePath = path.join(folderSource, file.name);
-        const stats = fs.statSync(filePath);
-        if (stats.isFile() && file.name.includes('.css')) {
-          const content = fs.readFileSync(filePath, 'utf-8');
-          arr.push(content);
-        };
-      });
-      fs.writeFile(folderPath, arr.join('\n'), (error) => {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log('Data written to file');
-        }
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if (err) {
+            console.error("Error reading file:", err);
+            return;
+          }
+          fs.stat(filePath, (err, stats) => {
+            if (err) {
+              console.error(err);
+            }
+            if (stats.isFile() && file.name.includes('.css')) {
+              arr.push(data);
+            };
+            if (arr.length === files.filter(f => f.isFile() && f.name.includes('.css')).length) {
+              fs.writeFile(folderPath, arr.join('\n'), 'utf8', (err) => {
+                if (err) {
+                  console.error("Error writing file:", err);
+                };
+              });
+            }
+          });
+        });
       });
     };
   });
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
+/* assets */
 const assetsSource = path.join(__dirname, 'assets');
 const assetsPath = path.join(__dirname, 'project-dist', 'assets');
 
+/* styles */
 let stylesData = []
 const styleSource = path.join(__dirname, 'styles');
 const stylePath = path.join(__dirname, 'project-dist', 'style.css');
 
+/* templates */
 const indexSource = path.join(__dirname, 'template.html');
 const indexPath = path.join(__dirname, 'project-dist', 'index.html');
+const compPath = path.join(__dirname, 'components');
+
 
 fs.access(assetsPath, fs.constants.F_OK, (err) => {
   if (!err) {
@@ -69,8 +74,10 @@ function copyAssets() {
             });
         });
         console.log('Assets copied successfully.');
-        copyStyles();
+
       };
+      copyStyles();
+      copyIndex();
     });
 }
 
@@ -83,44 +90,69 @@ function copyStyles() {
       else {
         files.forEach(file => {
           const filePath = path.join(styleSource, file.name);
-          const stats = fs.statSync(filePath);
-          if (stats.isFile() && file.name.includes('.css')) {
-            const content = fs.readFileSync(filePath, 'utf-8');
-            stylesData.push(content);
-          };
-        });
-        fs.writeFile(stylePath, stylesData.join('\n'), (error) => {
-          if (error) {
-            console.error(error);
-          } else {
-            console.log('Styles data written to file');
-          }
+          fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+              console.error("Error reading file:", err);
+            }
+            fs.stat(filePath, (err, stats) => {
+              if (err) {
+                console.error(err);
+              }
+              if (stats.isFile() && file.name.includes('.css')) {
+                stylesData.push(data);
+              };
+              if (stylesData.length === files.filter(f => f.isFile() && f.name.includes('.css')).length) {
+                fs.writeFile(stylePath, stylesData.join('\n'), 'utf8', (err) => {
+                  if (err) {
+                    console.error("Error writing file:", err);
+                  };
+                });
+              };
+            });
+          });
         });
       };
     });
-}
+  };
+
 function copyIndex() {
-  fs.readdir(indexSource,
+  fs.readFile(indexSource, 'utf8', (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+    }
+    let filesCount = 0;
+    fs.readdir(compPath,
     { withFileTypes: true },
     (err, files) => {
       if (err)
         console.log(err);
       else {
         files.forEach(file => {
-          const filePath = path.join(indexSource, file.name);
-          const stats = fs.statSync(filePath);
-          if (stats.isFile() && file.name.includes('.css')) {
-            const content = fs.readFileSync(filePath, 'utf-8');
-            stylesData.push(content);
-          };
-        });
-        fs.writeFile(stylePath, stylesData.join('\n'), (error) => {
-          if (error) {
-            console.error(error);
-          } else {
-            console.log('Styles data written to file');
-          }
+          const filePath = path.join(compPath, file.name);
+          fs.readFile(filePath, 'utf8', (err, tempData) => {
+            if (err) {
+              console.error("Error reading file:", err);
+            }
+            fs.stat(filePath, (err, stats) => {
+            if (err) {
+              console.error(err);
+            }
+            if (stats.isFile() && file.name.includes('.html')) {
+              let templ = `${file.name.replace('.html', '')}`;
+              data = data.replace(`{{${templ}}}`,`${tempData}`);
+              filesCount += 1;
+            };
+            if (filesCount === files.filter(f => f.isFile() && f.name.includes('.html')).length) {
+              fs.writeFile(indexPath, data, 'utf8', (err) => {
+                if (err) {
+                  console.error("Error writing file:", err);
+                }
+              });
+            };
+          });
+          });
         });
       };
     });
+  });
 }
